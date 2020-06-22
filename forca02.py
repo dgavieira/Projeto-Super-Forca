ec8a9eda92472ee23bb9e8e3076ab6a0b099e22d
Diego Vieira <dgavieira@users.noreply.github.com>
2020-06-22 11:46:20 -0400

    Arquivos de esqueleto

---
+++ forca02.py
@@ -0,0 +1,18 @@
+import random
+
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
