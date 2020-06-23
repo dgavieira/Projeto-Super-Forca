26225d2ea16a88b63f0e8756bfa35088ac1110ef
Diego Vieira <dgavieira@users.noreply.github.com>
2020-06-23 14:11:35 -0400

    Trabalho finalizado

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Projeto-Super-Forca
  Projeto Final da disciplina de programação Python do Projeto Super
+
+ Alunos:
+ Diego Giovanni de Alcântara Vieira
+ Alex Rocha
+ Thayla Moura
+
+ #Arquivos
+
+ forca.py - Arquivo código fonte do jogo da forca
+ base.txt - Arquivo base de dados com as palavras do jogo
+ jogo.txt - Arquivo de resumo da partida jogada. Armazena todas as partidas jogadas até então
--- forca.py
+++ forca.py
@@ -1,155 +1,132 @@
+# -*- coding: utf-8 -*-
+#Projeto Super
+#Task: WP3-CETELI-2-IA
+#Curso: Programação Python
+#Bolsista 1: Alex Rocha
+#Bolsista 2: Diego Vieira
+#Bolsista 3: Thayla Moura
+
 import random
 
-def jogar():
-    imprime_mensagem_abertura()
-    palavra_secreta = carrega_palavra_secreta()
+def criarBase(nome):
+    #essa lista define as palavras do jogo
+    palavras = "banana acerola caju laranja verde vermelho azul rosa carro moto barco casa cidade aula viajem pai comida aviao gato pizza escola"
+    #esse bloco criar um arquivo nome.txt escreve a string palavras
+    base = open(nome, 'w')
+    base.write(palavras)
+    base.close()
 
-    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+#essa função recupera as palavras do arquivo nome.txt em uma variável palavras
+def listarPalavras(nome):
+    base = open(nome, 'r')
+    palavras = base.read().split()
+    base.close()
+    return palavras
 
-    enforcou = False
-    acertou = False
-    erros = 0
+#essa função cria os parâmetros do jogo
+def criarJogo():
+    nome = "base.txt"
+    criarBase(nome)
+    palavra = random.choice(listarPalavras(nome))
+    montagem = []
+    for n in range(len(palavra)):
+        montagem.append("-")
+    jogo = dict(palavra = palavra, montagem = montagem, acertos = [], erros = [], quantPalpites = 0, terminou = False, ganhou = False)
+    return jogo
 
-    print(letras_acertadas)
+#essa função pega os dados do jogador da partida
+def criarJogador():
 
-    while(not enforcou and not acertou):
-        chute = pede_chute()
+    nome = str(input("Insira seu nome: "))
+    email = str(input("Insira seu email: "))
+    jogador = dict(nome = nome, email = email)
+    print("")
+    return jogador
 
-        if (chute in palavra_secreta):
-            marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+#essa funlão valida a entrada do usuário em termos de palpites no jogo
+def validarEntrada(letra, acertos):
+    if(len(letra) > 1):
+        print("Insira apenas uma letra!")
+        return False
+    elif(not letra.isalpha()):
+        print("Insira uma letra do alfabeto!")
+        return False
+    elif(letra in acertos):
+        print("Insira uma letra diferente!")
+        return False
+    else: return True
+
+#essa função permite ao jogador fazer uma jogada
+def fazerJogada(jogo):
+    if(len(jogo['erros']) <= 5):
+        letra = str(input("\nInsira uma letra: ").lower())
+        if(not validarEntrada(letra, jogo['acertos'])):
+            fazerJogada(jogo)
+        elif(letra not in jogo['palavra']):
+            jogo['erros'].append(letra)
+            print("A letra inserida não ocorre na palavra.\n")
         else:
-            erros = erros + 1
-            desenha_forca(erros)
-
-        enforcou = erros == 7
-        acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
-
-    if(acertou):
-        imprime_mensagem_vencedor()
+            jogo['acertos'].append(letra)
+            print("Você acertou uma letra!\n")
+            for n in range(len(jogo['palavra'])):
+                if(jogo['palavra'][n] == letra): jogo['montagem'][n] = letra
+            if(jogo['palavra'] == "".join(jogo['montagem'])):
+                print("Parabéns, você acertou todas as letras da palavra e terminou o jogo!\n")
+                jogo['terminou'] = jogo['ganhou'] = True
     else:
-        imprime_mensagem_perdedor(palavra_secreta)
-    print("Fim do jogo")
+        print("Você já usou todas sua tentativas.\n")
+        jogo['terminou'] = True
 
+#essa função permite ao jogador fazer um palpite
+def fazerPalpite(jogo):
+    if(jogo['quantPalpites'] <= 3):
+        palpite = str(input("\nInsira seu palpite: ").lower())
+        if(palpite == jogo['palavra']):
+            print("Parabéns, você acertou a palavra e terminou o jogo!\n")
+            jogo['terminou'] = jogo['ganhou'] = True
+        else:
+            jogo['quantPalpites'] += 1
+            print("Palpite incorreto.")
+    else:
+        print("Você já usou todos seus palpites.\n")
+        jogo['terminou'] = True
 
-def imprime_mensagem_abertura():
-    print("****************************************")
-    print("****Bem vindo ao jogo da Forca**********")
-    print("****************************************")
+#essa função mostra o resumo da partida
+def imprimirResumo(jogo):
+    print("Palavra: ", end = "")
+    for letra in jogo['montagem']:
+        if(letra == '-'): print("_ ", end = "")
+        else: print("%s " % letra, end = "")
+    print("\n\nLetras acertadas: ", jogo['acertos'])
+    print("Letras erradas: ", jogo['erros'])
+    print("Tentativas de letra disponíveis: ", 5 - len(jogo['erros']))
+    print("Palpites disponíveis: ", 3 - jogo['quantPalpites'])
 
-def carrega_palavra_secreta():
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-
-    for linha in arquivo:
-        linha = linha.strip()
-        palavras.append(linha)
-
+#essa função salva o resultado do jogo num arquivo
+def salvarJogo(jogo, jogador):
+    arquivo = open("jogo.txt", 'a')
+    arquivo.write(jogador['nome'] + "\n")
+    arquivo.write(jogador['email'] + "\n")
+    arquivo.write(jogo['palavra'] + "\n")
+    arquivo.write("Ganhou: " + str(jogo['ganhou']) + "\n\n")
     arquivo.close()
 
-    numero = random.randrange(0, len(palavras))
-    palavra_secreta = palavras[numero].upper()
-    return palavra_secreta
+#essa é a função principal do jogo da forca
+def main():
+    jogo = criarJogo()
+    print("Bem-vindo ao jogo de forca em Python!\n")
+    jogador = criarJogador()
+    imprimirResumo(jogo)
+    fazerJogada(jogo)
+    imprimirResumo(jogo)
 
-def inicializa_letras_acertadas(palavra):
-    return ["_" for letra in palavra]
+    while(not jogo['terminou']):
+        res = str(input("\nGostaria de tentar mais uma letra ou tentar um palpite da palavra? (L/P): ").upper())
+        if(res == 'L'):
+            fazerJogada(jogo)
+            imprimirResumo(jogo)
+        elif(res == 'P'): fazerPalpite(jogo)
+        else: print("Opção inválida!")
+    salvarJogo(jogo, jogador)
 
-def pede_chute():
-    chute = input("Qual letra?")
-    chute = chute.strip().upper()
-    return chute
-
-def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
-    index = 0
-    for letra in palavra_secreta:
-        if (chute == letra):
-            letras_acertadas[index] = letra
-        index = index + 1
-
-def imprime_mensagem_vencedor():
-    def imprime_mensagem_vencedor():
-        print("Parabéns, você ganhou!")
-        print("       ___________      ")
-        print("      '._==_==_=_.'     ")
-        print("      .-\\:      /-.    ")
-        print("     | (|:.     |) |    ")
-        print("      '-|:.     |-'     ")
-        print("        \\::.    /      ")
-        print("         '::. .'        ")
-        print("           ) (          ")
-        print("         _.' '._        ")
-        print("        '-------'       ")
-
-def imprime_mensagem_perdedor(palavra_secreta):
-    print("Puxa, você foi enforcado!")
-    print("A palavra era {}".format(palavra_secreta))
-    print("    _______________         ")
-    print("   /               \       ")
-    print("  /                 \      ")
-    print("//                   \/\  ")
-    print("\|   XXXX     XXXX   | /   ")
-    print(" |   XXXX     XXXX   |/     ")
-    print(" |   XXX       XXX   |      ")
-    print(" |                   |      ")
-    print(" \__      XXX      __/     ")
-    print("   |\     XXX     /|       ")
-    print("   | |           | |        ")
-    print("   | I I I I I I I |        ")
-    print("   |  I I I I I I  |        ")
-    print("   \_             _/       ")
-    print("     \_         _/         ")
-    print("       \_______/           ")
-
-def desenha_forca(erros):
-    print("  _______     ")
-    print(" |/      |    ")
-
-    if(erros == 1):
-        print(" |      (_)   ")
-        print(" |            ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 2):
-        print(" |      (_)   ")
-        print(" |      \     ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 3):
-        print(" |      (_)   ")
-        print(" |      \|    ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 4):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |            ")
-        print(" |            ")
-
-    if(erros == 5):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |            ")
-
-    if(erros == 6):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |      /     ")
-
-    if (erros == 7):
-        print(" |      (_)   ")
-        print(" |      \|/   ")
-        print(" |       |    ")
-        print(" |      / \   ")
-
-    print(" |            ")
-    print("_|___         ")
-    print()
-
-
-if __name__ == '__main__':
-    jogar()
+main()
